620e96abbbdb7b0d8a90dde2e7bce4a681525fea
matus1888 <matus1888@gmail.com>
2025-04-05 21:54:05 +0300

    feat(CORS): enable all origins to testing

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    //TODO enable ghpage for my appllication
+    origin: '*', // Разрешённые домены
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // Разрешённые HTTP-методы
+    allowedHeaders: 'Content-Type, Authorization', // Разрешённые заголовки
+    credentials: true, // Разрешить отправку куки и заголовков авторизации
+  });
   await app.listen(process.env.PORT ?? 4000);
 }
 bootstrap();
